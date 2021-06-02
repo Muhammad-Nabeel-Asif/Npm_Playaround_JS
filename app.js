f498183b41b38922471c6cbccc6ec13fe1db03dd
Nabeel Asif <nabeel.asif362@gmail.com>
2021-06-03 00:07:00 +0500

    module exports

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const math = require("./math");
-console.log(math);
+console.dir(math);
+
+// This is how we are using methods and functions from other file "math.js"
+console.log("Addition is : ", math.add(15, 20));
+console.log("Square is : ", math.square(20));
--- math.js
+++ math.js
@@ -1,9 +1,20 @@
 const add = (x, y) => x + y;
-
 const PI = 3.14159;
-
 const square = (x) => x * x;
 
+// ==================================================
+// "module.exports.add" copies the values from variables , constants and other objects , that can be used in other files , e.g in this directory , we are exporting (add , PI and square) to "app.js" file. This is how we can transfer copy of data from one file to another , as per usage and requirement.
+
+/*
 module.exports.add = add;
 module.exports.PI = PI;
 module.exports.square = square;
+*/
+
+// ===================================================
+
+// The above format could be made shorter with just :
+
+exports.square = square;
+exports.add = add;
+exports.PI = PI;
